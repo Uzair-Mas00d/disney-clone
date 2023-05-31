fff7a73ad6c16971dc6a868d4e75e53a6307cead
Uzair Masood <uzairmasood050@gmail.com>
2023-05-31 18:51:51 +0500

    ADD:image sliders

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import styled from 'styled-components'
+import Sliders from './Sliders'
+import Viweres from './Viweres'
 
 function Home() {
   return (
-    <div>
-        Home
-    </div>
+    <Container>
+      <Sliders />
+      <Viweres />
+    </Container>
   )
 }
 
 export default Home
+
+const Container = styled.main`
+  min-height:calc(100vh - 70px);
+  padding:0 calc(3.5vw + 5px);
+  position:relative;
+  overflow-x:hidden;
+
+  &:after{
+    content:'';
+    position:absolute; 
+    inset:0;
+    background:url('/images/home-background.png') center center / cover no-repeat fixed;
+    opacity:1;
+    z-index:-1;
+  }
+
+`
---
+++ src/components/Viweres.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Viweres() {
+  return (
+    <div>
+        Viweres
+    </div>
+  )
+}
+
+export default Viweres
